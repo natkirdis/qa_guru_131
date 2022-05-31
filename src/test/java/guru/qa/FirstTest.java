5ef8c00f1d18471ec9b8148fb53db4fd2e3c9c15
Natalia Kirdis <kirdisnatalia@gmail.com>
2022-05-31 22:16:44 +0300

    first commit

---
+++ src/test/java/guru/qa/FirstTest.java
@@ -0,0 +1,14 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FirstTest {
+
+    @Test
+    void firstTest() {
+        assertTrue(2 + 2 == 4);
+    }
+}
